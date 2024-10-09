357a881e64429add5e3dc0041843864d4e9294c2
islem969 <islem.amor@esprit.tn>
2024-10-09 19:12:51 +0100

    ajout commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -25,7 +25,7 @@ public class User implements Serializable {
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
 	
-	private String firstName; 
+	public String firstName; 
 	
 	private String lastName;
 	
